6e2c3176e5891157369f05d837dc58dc0cfd2fa5
super020gamer <111883161+super020gamer@users.noreply.github.com>
2024-02-15 20:45:14 +0100

    Update vendorsetup.sh V2

--- nonetwrp/device/incar/NpadAir/vendorsetup.sh
+++ nonetwrp/device/incar/NpadAir/vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_NpadAir-user
-add_lunch_combo twrp_NpadAir-userdebug
-add_lunch_combo twrp_NpadAir-eng
+add_lunch_combo omni_NpadAir-user
+add_lunch_combo omni_NpadAir-userdebug
+add_lunch_combo omni_NpadAir-eng
